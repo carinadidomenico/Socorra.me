e1a46e8bbe62dcf5d00883181aeb3961dd99e753
carinadidomenico <carinadidomenico@gmail.com>
2019-07-10 15:46:28 -0300

    Criar script

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+// VARIÁVEIS
+
+let menuHamburguer = document.querySelector (".nav__menu-hamburguer");
+let menu = document.querySelector ("nav__menu");
+
+
+menuHamburguer.addEventListener ("click", function () {
+    menu.classList.toggle ("nav__menu--active");
+}) 
+
+
+function card () {
+    
+    document.querySelector (".card").innerHTML +=
+}
