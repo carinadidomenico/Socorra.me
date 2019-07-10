9862bdb2cef81ecf7a06f9e7352398c7f3c90cf7
carinadidomenico <carinadidomenico@gmail.com>
2019-07-10 17:23:47 -0300

    Infos

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,21 @@
 // VARIÁVEIS
 
+let lista = lista.caso;
 let menuHamburguer = document.querySelector (".nav__menu-hamburguer");
 let menu = document.querySelector ("nav__menu");
+
+
+const insertCard = (e) => {
+    let card = [];
+    e.
+}
 
 
 menuHamburguer.addEventListener ("click", function () {
     menu.classList.toggle ("nav__menu--active");
 }) 
 
-
 function card () {
-    
-    document.querySelector (".card").innerHTML +=
 }
+
+document.getElementsByClassName ("card").innerHTML = `<h1>OI</h1>`
