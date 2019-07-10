37009175d1984e839e707af1b279f0ed6f0d78f1
carinadidomenico <carinadidomenico@gmail.com>
2019-07-10 18:16:25 -0300

    Tentando arrumar o menu

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,21 +1,7 @@
-// VARIÁVEIS
-
-let lista = lista.caso;
 let menuHamburguer = document.querySelector (".nav__menu-hamburguer");
-let menu = document.querySelector ("nav__menu");
+let menu = document.querySelector (".nav__menu");
 
-
-const insertCard = (e) => {
-    let card = [];
-    e.
-}
-
-
-menuHamburguer.addEventListener ("click", function () {
-    menu.classList.toggle ("nav__menu--active");
-}) 
-
-function card () {
-}
-
-document.getElementsByClassName ("card").innerHTML = `<h1>OI</h1>`
+menuHamburguer.addEventListener ('click', function () {
+    console.log ('FUNCIONA OPORA');
+    menu.style.display = "flex";
+})
