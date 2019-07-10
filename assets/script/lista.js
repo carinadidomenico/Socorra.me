88b6a54b8eb7f2211c94468c9ab63107c7eec816
carinadidomenico <carinadidomenico@gmail.com>
2019-07-10 17:06:26 -0300

    Criar lista dos casos

---
+++ assets/script/lista.js
@@ -0,0 +1,40 @@
+const card = {
+    "caso" : [
+      {
+        "condicao":"queimaduras",
+        "grau": "3",
+        "comoProceder":"Deixar a ferida em água corrente, fria. Se houver alguma pomada para queimaduras, aplicar.",
+        "naoFazer":"Não colocar pasta de dente."
+      },
+      {
+        "condicao":"engasgamento",
+        "grau": "4",
+        "comoProceder":"Deixar a ferida em água corrente, fria. Se houver alguma pomada para queimaduras, aplicar.",
+        "naoFazer":"Não dar água para a pessoa beber."
+      },
+      {
+        "condicao":"choque elétrico",
+        "grau": "grave",
+        "comoProceder":"Desligar a chave geral e chamar uma ambulância.",
+        "naoFazer":"Não encostar na pessoa."
+      },
+      {
+        "condicao":"Quedas",
+        "grau": "de 3 a 5",
+        "comoProceder":"Se, após a queda a pessoa estiver inconsciente, não encostar na pessoa e chamar uma ambulância",
+        "naoFazer":"Não encostar na pessoa.",
+      },
+      {
+        "condicao":"Febre",
+        "grau": "de 3 a 5",
+        "comoProceder":"Se, após a queda a pessoa estiver inconsciente, não encostar na pessoa e chamar uma ambulância",
+        "naoFazer":"Não encostar na pessoa.",
+      },
+      {
+        "condicao":"Dor de cabeça",
+        "grau": "2",
+        "comoProceder":"Tomar algum analgésico que esteja acostumado. Se a dor persistir, procurar um médico",
+        "naoFazer":"Tomar vários remédios de uma vez",
+      },      
+    ]
+  }
