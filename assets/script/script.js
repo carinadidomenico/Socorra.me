c757f2b68f83e2f887313f34cdaddcde8fd0d658
carinadidomenico <carinadidomenico@gmail.com>
2019-07-11 17:18:38 -0300

    Renderizando cards

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,13 +15,58 @@ menuHamburguer.addEventListener ('click', function () {
 
 // FORM
 
+const listaCasos = JSON.stringify(card)
+const listaCasosJs = JSON.parse(listaCasos)
 let search = document.querySelector (".search");
-let lista = card.caso
+let searchText = document.querySelector (".search__text");
+
+const insertCard = (card) => {
+
+    let cards = ''
+    card.forEach(caso => {
+        
+        cards += `
+        <div>
+        <h2>${caso.condicao}</h2>
+          <p>${caso.grau}</p>
+          <p>${caso.comoProceder}</p>
+          <p>${caso.naoFazer}</p>
+          </div>
+          `
+        });
+
+        return cards
+    }
+
+    document.querySelector(".card").innerHTML = insertCard(listaCasosJs.caso)
+
 
 search.addEventListener('submit', function(e) {
     e.preventDefault();
-    lista.forEach(element => {
-        console.log ("oi")
-    });
 
+    console.log (searchText.value)
+
+
+    
 }) 
+
+
+
+
+        // const orderList = (caso1, caso2, propriedade) => {
+        //     if(caso1[propriedade].toLowerCase() < caso2[propriedade].toLowerCase()){
+        //       return -1
+        //     }
+        //     if(caso1[propriedade].toLowerCase() > caso2[propriedade].toLowerCase()){
+        //       return 1
+        //     }
+        //     return 0
+        //   }
+          
+        //   const ordenarCard = document.getElementById("ordenar-livro")
+          
+        //   ordenarCard.addEventListener("click", (e) => {
+        //     const listaOrdenada = listaCasos.caso.sort((caso1, caso2) => orderList(caso1, caso2, "livro"))
+        //     document.querySelector (".card").innerHTML = insertCard(listaOrdenada)
+        //   })
+          
