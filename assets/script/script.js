47f9976c546fa6931c1004655e8bd88ba39ef5d6
carinadidomenico <carinadidomenico@gmail.com>
2019-07-11 00:14:56 -0300

    Dropdow menu

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,27 @@
+// MENU
+
 let menuHamburguer = document.querySelector (".nav__menu-hamburguer");
 let menu = document.querySelector (".nav__menu");
 
 menuHamburguer.addEventListener ('click', function () {
-    console.log ('FUNCIONA OPORA');
-    menu.style.display = "flex";
+    if (menu.style.height){
+        menu.style.height = null;        
+    } else {
+        menu.style.height = menu.scrollHeight + "px";
+    }
+    
 })
+
+
+// FORM
+
+let search = document.querySelector (".search");
+let lista = card.caso
+
+search.addEventListener('submit', function(e) {
+    e.preventDefault();
+    lista.forEach(element => {
+        console.log ("oi")
+    });
+
+}) 
