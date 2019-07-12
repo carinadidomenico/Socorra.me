7a59abe6b31b198105afa433e0ac2344f14231b8
carinadidomenico <carinadidomenico@gmail.com>
2019-07-12 15:44:41 -0300

    Ordenando a lista

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,56 +13,73 @@ menuHamburguer.addEventListener ('click', function () {
 })
 
 
-// FORM
+// CARDS
 
 const listaCasos = card
-const search = document.querySelector (".search");
-const searchText = document.querySelector (".search__text");
+
+
+listaCasos.caso.sort((caso1, caso2) => {
+
+
+    if(caso1.condicao.toLowerCase() < caso2.condicao.toLowerCase()){
+        return -1
+    }
+    if(caso1.condicao.toLowerCase() > caso2.condicao.toLowerCase()){
+        return 1
+    } else {
+        return 0
+    } 
+    
+})
+
 
 const insertCard = (card) => {
-
+    
     let cards = ''
     card.forEach(caso => {
         
         cards += `
         <div class="card__container">
-            <div class= "card__content">
-                <h2>${caso.condicao}</h2>
-                <h3>Gravidade: ${caso.grau}</h3>
-                <h3>O que fazer: </h3>
-                <p>${caso.comoProceder}</p>
-                <h3>Cuidado!</h3>
-                <p>${caso.naoFazer}</p>
-            </div>
+        <div class= "card__content">
+        <h2>${caso.condicao}</h2>
+        <h3>Gravidade: ${caso.grau}</h3>
+        <h3>O que fazer: </h3>
+        <p>${caso.comoProceder}</p>
+        <h3>Cuidado!</h3>
+        <p>${caso.naoFazer}</p>
         </div>
-          `
-        });
-        return cards
-    }
+        </div>
+        `
+    });
+    return cards
+}
 
 document.querySelector(".card").innerHTML = insertCard(listaCasos.caso)
 
 
+// BUSCAR
+
+const search = document.querySelector (".search");
+const searchText = document.querySelector (".search__text");
+const searchButton = document.querySelector (".search__button")
+
+
 searchText.addEventListener('keyup', function(e) {
-    e.preventDefault();
+
     let filtro = listaCasos.caso.filter ((item) => {
         let searchInput = searchText.value.toLowerCase()
         return item.condicao.toLowerCase().includes(searchInput)
     })
 
     document.querySelector(".card").innerHTML = insertCard(filtro)    
-}) 
+})
 
 
-const orderList = (caso1, caso2, propriedade) => {
+searchButton.addEventListener('click', function(e){
+    e.preventDefault()
 
-    if(caso1[propriedade].toLowerCase() < caso2[propriedade].toLowerCase()){
-        return -1
-    }
-    if(caso1[propriedade].toLowerCase() > caso2[propriedade].toLowerCase()){
-        return 1
-    }
-        return 0
-}
+    document.querySelector(".card").innerHTML = insertCard(listaCasos.caso)
+})
 
-          
+
+
