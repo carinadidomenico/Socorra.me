14681d066da97aca7bd5e7452938d30d2df6727d
carinadidomenico <carinadidomenico@gmail.com>
2019-08-09 00:26:47 -0300

    Passando o site para react

---
+++ socorra.me react/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './logo.svg';
+import Header from './componentes/menu/Header';
+
+class App extends React.Component {
+  <Header/>
+}
+
+export default App;
---
+++ socorra.me react/src/componentes/menu/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    constructor () {
+
+    }
+    render () {
+        return (
+            <p>oi</p>
+        )
+    }
+}
